782ff63f3ca7a7eecf4bddca9626907528512ace
Linas Linartas <linas.linartas@nfq.lt>
2018-11-27 17:17:27 +0200

    Fixed MemoizeDecorator type errors.

--- src/Decorator/MemoizeDecorator.php
+++ src/Decorator/MemoizeDecorator.php
@@ -5,6 +5,7 @@ namespace LiLinen\Decor\Decorator;
 use LiLinen\Decor\Decoration\DecorationInterface;
 use LiLinen\Decor\Decoration\Memoize;
 use LiLinen\Decor\Decorator\Memoize\KeyMakerInterface;
+use LiLinen\Decor\Exception\DecorationException;
 use Psr\SimpleCache\CacheInterface;
 
 class MemoizeDecorator implements DecoratorInterface
@@ -43,9 +44,15 @@ class MemoizeDecorator implements DecoratorInterface
      * @param DecorationInterface|Memoize $decoration
      *
      * @return callable|null
+     *
+     * @throws DecorationException
      */
     public function getBeforeFunction(DecorationInterface $decoration): ?callable
     {
+        if (!($decoration instanceof Memoize)) {
+            throw new DecorationException('Decoration '.\get_class($decoration).' must be instanceof Memoize!');
+        }
+
         return function ($proxy, $instance, $method, $params, &$returnEarly) {
             $key = $this->keyMaker->makeKey($instance, $method, $params);
 
@@ -63,9 +70,15 @@ class MemoizeDecorator implements DecoratorInterface
      * @param DecorationInterface|Memoize $decoration
      *
      * @return callable|null
+     *
+     * @throws DecorationException
      */
     public function getAfterFunction(DecorationInterface $decoration): ?callable
     {
+        if (!($decoration instanceof Memoize)) {
+            throw new DecorationException('Decoration '.\get_class($decoration).' must be instanceof Memoize!');
+        }
+
         return function ($proxy, $instance, $method, $params, $returnValue, & $returnEarly) use ($decoration) {
             $key = $this->keyMaker->makeKey($instance, $method, $params);
 
